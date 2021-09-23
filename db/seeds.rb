bf2eb37cc9d9d88fb340084a5e8de87859961934
ZaidYounan <zaid.younan@gmail.com>
2021-09-23 20:37:06 +1000

    Install Faker, add 100 Fake entries to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do 
+    Book.create({
+        title: Faker::Book.unique.title,
+        author: Faker::Book.unique.name,
+        read: 'Yes',
+        rating: Faker::Book.random = Random.new(10)
+    })
+end
